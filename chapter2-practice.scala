d25e1a10524b8c0696cbd5601b5cf0d8b54123b3
DinoZhang <victordinozhang@gmail.com>
2017-03-07 10:40:01 +0800

    practice

---
+++ chapter2-practice.scala
@@ -0,0 +1,26 @@
+// q1
+def cal(x: Int) : Unit = {
+    if(x >0){
+        println(x +":"+ 1)
+    }else if(x == 0){
+        println(x +":"+ 0)
+    }else{
+        println(x +":"+ -1)
+    }
+}
+cal(5)
+cal(0)
+cal(-1)
+
+// q2
+def q2() ={
+}
+q2()
+
+// q4
+def q4(){
+   for (i <- 0 to 10 reverse)
+       println(i)
+
+} 
+q4()
