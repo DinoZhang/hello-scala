9211de002e208ff7a0222d7f2a0b736cb95dba5e
DinoZhang <victordinozhang@gmail.com>
2017-03-28 18:33:41 +0800

    update

--- scala-for-the-impatient/chapter5-class.scala
+++ scala-for-the-impatient/chapter5-class.scala
@@ -19,3 +19,28 @@ class Person{
 val p = new Person()
 println(p.age)
 
+class Message {
+    val time = new java.util.Date
+}
+val m = new Message
+println(m.time)
+
+
+class PersonCon{
+    private var name = ""
+    private var age = 0
+   
+   // 辅助构造器，必须调用主构造器，或其他辅助构造器
+   def this(name : String){
+       this()
+       this.name = name   
+    }
+   
+   def this(name : String,age : Int){
+       this(name)
+       this.age = age
+   }
+}
+val personCon= new PersonCon("jack",26)
+//println(personCon.name +":"+ personCon.age)  
+println(personCon)
